e933c845ee6b2c81dfd836a530a531a005ac6364
Parth21234 <chauhanparth974@gmail.com>
2025-11-13 13:53:51 +0530

    Functions of Buck and the values

--- Buck.py
+++ Buck.py
@@ -48,5 +48,25 @@
 """
 
 import functions_Buck as functions
+from functions_Buck import inductor_ripple_current, inductor_selection
 
-maximum_Duty_Cycle = functions.max_duty_cycle()
+v_out = float(input())
+i_out_max = float(input())
+v_in_max = float(input())
+switching_frequency = float(input())
+efficiency = float(input())
+
+maximum_Duty_Cycle = functions.max_duty_cycle(v_in_max, v_out, efficiency)
+print(maximum_Duty_Cycle)
+
+
+inductor_val = inductor_selection(v_in_max, v_out, switching_frequency, i_out_max)
+print(inductor_val)
+
+inductor_ripple_current = inductor_ripple_current(v_in_max, v_out, maximum_Duty_Cycle,
+                                                 switching_frequency, inductor_val)
+print(inductor_ripple_current)
+
+app_specificMax_switch_current = inductor_ripple_current/2 + i_out_max
+print(app_specificMax_switch_current)
+
--- functions_Buck.py
+++ functions_Buck.py
@@ -4,4 +4,23 @@ def max_duty_cycle(input_max_voltage, output_voltage, n = 90):
     return max_d
 
 
-def inductor_ripple_current(input_max_voltage, output_voltage, n = 90):
+def inductor_ripple_current(input_max_voltage, output_voltage, max_duty, switching_frequency, inductor_value):
+    deno = switching_frequency * inductor_value#L
+    del_inductor_ripple_current = ((input_max_voltage - output_voltage) * max_duty)/deno
+    return del_inductor_ripple_current
+
+def inductor_selection(input_max_voltage, output_voltage, switching_frequency, i_out_max):
+    deno = i_out_max * 0.3 * switching_frequency * input_max_voltage
+    inductor_value = output_voltage * (input_max_voltage - output_voltage)/deno
+    return inductor_value
+
+def rectifier_diode_selection(output_voltage, switching_frequency, i_out_max, max_duty):
+    forward_current = i_out_max * (1 - max_duty)
+
+    power_of_dissipation = forward_current * 0.7  # Assuming, it is silicon diode.
+    return power_of_dissipation, forward_current
+
+def out_voltage_setting(i_forward):
+    i_r1_r2 = 100 * i_forward
+    """The current through the resistive divider needs to be at least 100 times as big as the feedback bias current"""
+
